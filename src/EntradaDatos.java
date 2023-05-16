6cb57147230be52fd21b81b993fba42c9ef9d731
USER-PC <USER-PC@DESKTOP-BHI57B9>
2023-05-16 23:43:49 +0200

    Añadidas clase de entrada de datos y clase donde se calcula el imc

---
+++ src/EntradaDatos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EntradaDatos {
+
+	
+	
+	Scanner n1 = new Scanner(System.in);
+    double altura = n1.nextDouble();
+
+    Scanner n2 = new Scanner(System.in);
+    double peso = n2.nextDouble();
+	
+    Scanner n3 = new Scanner(System.in);
+	double dosis = n3.nextDouble();
+	
+	
+	
+}
---
+++ src/IMC.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class IMC {
+
+int peso,altura;
+    
+double IMC = peso / ((altura/100)*(altura/100));
+
+    
+}
+
+
+
+
+
+
